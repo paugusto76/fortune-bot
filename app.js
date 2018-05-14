22a2005eea3d51f982b1d473d6b42f23bc02902f
Pedro Augusto <augusto.pedro@microsoft.com>
2018-05-14 17:28:08 +0100

    debug

--- app.js
+++ app.js
@@ -37,28 +37,43 @@ function randomIntInc (low, high) {
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    if (session.message.text) {
-        switch (session.message.text.toLowerCase().trim()) {
-            case 'fortune':
-            case 'fortune bot fortune':
-                
-            var index = randomIntInc(0, quotes.length - 1);
-
-    var msg = new builder.Message(session);
-    msg.attachmentLayout(builder.AttachmentLayout.carousel);
-    msg.attachments([
-        new builder.HeroCard(session)
-            .title('Fortune cookie')
-            .text(quotes[index])
-            .images([builder.CardImage.create(session, 'https://classroomclipart.com/images/gallery/Clipart/Food/Dessert_Clipart/fortune_cookie.jpg')])
-    ]);
-
-    session.send(msg).endDialog();
-
-                break;
-            default:
-                session.send(session.message.text);
-                break;
-        }
+    
+    var text = session.message.text;
+    if (session.message.entities) {
+        session.message.entities
+            .filter(entity => ((entity.type === "mention") && (entity.mentioned.id.toLowerCase() === botId)))
+            .forEach(entity => {
+                text = text.replace(entity.text, "");
+            });
+        text = text.trim();
     }
+
+    session.send(text);
+    
+    
+    
+    // if (session.message.text) {
+    //     switch (session.message.text.toLowerCase().trim()) {
+    //         case 'fortune':
+    //         case 'fortune bot fortune':
+                
+    //             var index = randomIntInc(0, quotes.length - 1);
+
+    //             var msg = new builder.Message(session);
+    //             msg.attachmentLayout(builder.AttachmentLayout.carousel);
+    //             msg.attachments([
+    //                 new builder.HeroCard(session)
+    //                     .title('Fortune cookie')
+    //                     .text(quotes[index])
+    //                     .images([builder.CardImage.create(session, 'https://classroomclipart.com/images/gallery/Clipart/Food/Dessert_Clipart/fortune_cookie.jpg')])
+    //             ]);
+
+    //             session.send(msg).endDialog();
+
+    //             break;
+    //         default:
+    //             session.send(session.message.text.toLowerCase().trim());
+    //             break;
+    //     }
+    // }
 });
