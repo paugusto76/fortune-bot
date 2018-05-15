93896beb627b5f7b344a1d871f7c4f12646b1ad5
Pedro Augusto <paugusto76@gmail.com>
2018-05-15 16:51:27 +0100

    update

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
     try{
         if (session.message.entities) {
             session.message.entities
-                .filter(entity => ((entity.type === "mention") && (entity.mentioned.id.toLowerCase() === session.message.bot.id)))
+                .filter(entity => ((entity.type === "mention") && (entity.mentioned.id.toLowerCase() === session.message.address.bot.id)))
                 .forEach(entity => {
                     text = text.replace(entity.text, "");
                 });
